9ecbf8deba97e0accd8f0deacdbfd6a07aa3d70b
sameer-malik1 <160597574+sameer-malik1@users.noreply.github.com>
2024-11-11 12:59:16 +0500

    add footer section in Home page

--- src/components/HomePage/Footer.jsx
+++ src/components/HomePage/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {FooterLink2} from "../../data/footer-links";
+import { FooterLink2 } from "../../data/footer-links";
 import logo from "../../assets/Logo/Logo-Full-Light.png";
 import { Link } from "react-router-dom";
 import { PiGoogleLogoFill } from "react-icons/pi";
@@ -8,10 +8,10 @@ import { FaFacebook } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 
 function Footer() {
-  console.log('data fotter: ',FooterLink2);
+  console.log("data fotter: ", FooterLink2);
   return (
     <div className="w-11/12   mx-auto  ">
-      <div className="flex gap-10 py-5  justify-evenly  ">
+      <div className="flex gap-10 py-5 pt-12  justify-evenly  ">
         {/* 1 columns */}
         <div className="text-richblack-500 flex flex-col gap-2">
           <img src={logo} alt="logo" />
@@ -29,7 +29,7 @@ function Footer() {
               <FaFacebook size={25} />
             </Link>
             <Link to="#">
-              <PiGoogleLogoFill size={25}/>
+              <PiGoogleLogoFill size={25} />
             </Link>
             <Link to="#">
               <AiFillTwitterCircle size={25} />
@@ -38,11 +38,10 @@ function Footer() {
               <FaYoutube size={25} />
             </Link>
           </div>
-          
         </div>
         {/* 2 columns */}
         <div className="text-richblack-500 flex flex-col gap-2">
-          <h2 className='text-white font-bold'>Resources</h2>
+          <h2 className="text-white font-bold">Resources</h2>
           <Link to={"#"}>
             <p>Articles</p>
           </Link>
@@ -67,7 +66,7 @@ function Footer() {
           <Link to={"#"}>
             <p>Work spaces</p>
           </Link>
-          <h2 className='text-white font-bold mt-6'>Support</h2>
+          <h2 className="text-white font-bold mt-6">Support</h2>
           <Link to={"#"}>
             <p>Help center</p>
           </Link>
@@ -75,7 +74,7 @@ function Footer() {
 
         {/* 3 columns */}
         <div className="text-richblack-500 flex flex-col gap-2">
-          <h2 className='text-white font-bold'>Plans</h2>
+          <h2 className="text-white font-bold">Plans</h2>
           <Link to={"#"}>
             <p>Paid membership</p>
           </Link>
@@ -85,8 +84,8 @@ function Footer() {
           <Link to={"#"}>
             <p>Business solutions</p>
           </Link>
-          
-          <h2 className='text-white font-bold mt-6'>Community</h2>
+
+          <h2 className="text-white font-bold mt-6">Community</h2>
           <Link to={"#"}>
             <p>Forums</p>
           </Link>
@@ -96,35 +95,37 @@ function Footer() {
           <Link to={"#"}>
             <p>Events</p>
           </Link>
-         
         </div>
-        <div className='border-solid  border-richblack-700 border-[1px]'></div>
+        <div className="border-solid  border-richblack-700 border-[1px]"></div>
         {/* 4 columns */}
-        {
-          FooterLink2.length > 0 ? FooterLink2.map((col,index)=>(<div className="text-richblack-500 flex flex-col gap-2">
-            <h2 className='text-white font-bold'>{col.title}</h2>
-            {
-              col.links.map(link=>(<Link to={link.link}><p>{link.title}</p></Link>))
-            }
-            
-          </div>)) : null
-        }
+        {FooterLink2.length > 0
+          ? FooterLink2.map((col, index) => (
+              <div className="text-richblack-500 flex flex-col gap-2">
+                <h2 className="text-white font-bold">{col.title}</h2>
+                {col.links.map((link) => (
+                  <Link to={link.link}>
+                    <p>{link.title}</p>
+                  </Link>
+                ))}
+              </div>
+            ))
+          : null}
         {/* 5 columns */}
         {/* 6 columns */}
       </div>
       <div className="flex justify-between w-full py-6 text-richblack-500 border-solid  border-richblack-700 border-t-[1px]">
         <div className="flex gap-4 ">
-          <Link to={'#'}>
-          <p>Privacy Policy</p>
+          <Link to={"#"}>
+            <p>Privacy Policy</p>
           </Link>
           <div className="border-solid  border-richblack-700 border-[1px]"></div>
-          <Link to={'#'}>
-          <p>Cookie Policy</p>
+          <Link to={"#"}>
+            <p>Cookie Policy</p>
           </Link>
           <div className="border-solid  border-richblack-700 border-[1px]"></div>
 
-          <Link to={'#'}>
-          <p>Terms</p>
+          <Link to={"#"}>
+            <p>Terms</p>
           </Link>
         </div>
         <div>
