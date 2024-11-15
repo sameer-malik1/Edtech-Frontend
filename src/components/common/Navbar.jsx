598f35daf25f81022d59fa31dd6188a281676872
sameer-malik1 <dev.sameermalik@gmail.com>
2024-11-15 11:12:08 +0500

    add category dropdown for the instructor

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {NavbarLinks} from '../../data/navbar-links'
 import navLogo from '../../assets/Logo/Logo-Full-Light.png'
 import { Link, matchPath, useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { FaShoppingCart } from "react-icons/fa";
 import ProfileDropdown from '../Auth/ProfileDropdown'
+import { apiConnector } from '../../services/apiConnector'
+import { categories } from '../../services/api'
+import { IoIosArrowDown } from "react-icons/io";
+import axios from 'axios'
+
+const subLinks = [{title:"python",path:"/course/python"},{title:"Java",path:"/course/java"},]
 
 function Navbar() {
     const {user} = useSelector((state)=> state.profile);
     const {token} = useSelector((state)=> state.auth);
     const {totalItems} = useSelector((state)=> state.cart);
+    const [ssubLinks,setSsubLinks] = useState([]);
 
     const location = useLocation();
+
+    const fetchCategories = async () => {
+        try {
+            // const result = await apiConnector('GET',categories.CATEGORIES_API);
+            const result = await axios.get('http://localhost:3000/api/v1/course/showAllCategories');
+            console.log("category_list: ",result);
+            console.log("category_list_Data: ",result.data);
+            setSsubLinks(result.data.data);
+
+            
+        } catch (error) {
+            console.log(error);
+            console.log("Error while fetching categories");
+            
+        }
+    }
+
+    useEffect(()=>{
+        fetchCategories();
+    },[]);
+
     function matchRoute(route){
         return matchPath({path:route},location.pathname);
     }
@@ -31,7 +59,20 @@ function Navbar() {
                     NavbarLinks.map((link,index)=>(
                         <li key={index} >
                             {
-                                link.title === 'Catalog' ? (<div></div>) : 
+                                link.title === 'Catalog' ? (<div className='relative group flex items-center gap-1'>
+                                    <p>{link.title}</p>
+                                    <IoIosArrowDown />
+                                    <div className='invisible absolute left-[50%] top-[-100%] translate-x-[-50%] translate-y-[80%] 
+                                     flex flex-col lg:w-[250px] p-3 rounded bg-richblack-25 opacity-0 group-hover:visible group-hover:opacity-100'>
+                                    <div className='absolute left-[50%] top-0 translate-x-[80%] translate-y-[-45%] w-5 h-5 bg-richblack-25  rotate-45'></div>
+
+                                        {
+                                            subLinks?.map((link,index)=>(
+                                                <Link key={index} to={Link?.path}><p className='text-richblack-800'>{link?.title}</p></Link>
+                                            ))
+                                        }
+                                    </div>
+                                </div>) : 
                                 (
                                     <Link to={link?.path}>
                                         <p className={`${matchRoute(link?.path) ? "text-yellow-25" : 'text-richblack-25'}`}>{link.title}</p>
