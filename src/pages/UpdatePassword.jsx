8f4aaeaf4a756f92675b7f054496a42b76a3a599
sameer-malik1 <160597574+sameer-malik1@users.noreply.github.com>
2024-11-21 11:43:11 +0500

    add change password page

---
+++ src/pages/UpdatePassword.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { resetPassword } from '../services/operations/authAPI';
+import { useLocation } from 'react-router-dom';
+
+function UpdatePassword() {
+
+    const {loading} = useSelector(state=> state.auth);
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const [showPassword,setShowPassword] = useState(false);
+    const [showConfirmPassword,setShowConfirmPassword] = useState(false);
+
+    const [formData,setFormData] = useState({
+        password:"",
+        confirmPassword:""
+    })
+    function handleOnChange(e){
+        setFormData((prev)=> ({
+            ...prev,
+            [e.target.name]:e.target.value
+        }))
+    }
+
+    function handleOnSubmit(e){
+        e.preventDefault();
+        const token = location.pathname.split('/').at(-1);
+        dispatch(resetPassword(password,confirmPassword,token));
+
+    }
+  return (
+    <div>
+        {
+            loading ? (<div>Loading ...</div>) :
+            (
+                <div>
+                    <h1>Choose new Password</h1>
+                    <p>Almost Done, Enter a new Password and you're all set.</p>
+                    <form onSubmit={handleOnSubmit}>
+                        <label>
+                            <p>New Password*</p>
+                            <input type={showPassword ? "text" : "password"}
+                                required
+                                placeholder='Enter new Password'
+                                name='password'
+                                value={FormData.password}
+                                onChange={handleOnChange}
+                                 />
+
+                        </label>
+                        <label>
+                            <p>Confirm Password*</p>
+                            <input type={showConfirmPassword ? "text" : "password"}
+                                required
+                                placeholder='Enter Confirm Password'
+                                name='confirmPassword'
+                                value={FormData.confirmPassword}
+                                onChange={handleOnChange}
+                                 />
+
+                        </label>
+                        <button type='submit'>
+                            Reset Password
+                        </button>
+                    </form>
+                </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default UpdatePassword
